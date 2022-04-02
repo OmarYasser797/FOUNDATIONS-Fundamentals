866a268e6d509f9a3c1d50d31257d094382a0388
Ammar <100184977+ammaryasser797@users.noreply.github.com>
2022-04-02 22:30:22 +0200

    Delete challenge_Gussing_game.py

--- challenge_Gussing_game.py
+++
@@ -1,9 +0,0 @@
-myFavfood = "that frog"
-food = input("What's your favorite food? ")
-
-if food == myFavfood:
-    print("Yep! So amazing!")
-else:
-    print("Yuck! That's not it")
-
-print("Thanks for playing")
